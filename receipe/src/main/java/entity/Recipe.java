0122a1cb6e2be30b98d9b8acd211cd6cfd55ddd0
humang <43851194+humang@users.noreply.github.com>
2025-02-20 14:04:50 +0900

    Basic entity implementation
    유저, 레시피, 레시피 절차 엔티티 작성

---
+++ receipe/src/main/java/entity/Recipe.java
@@ -0,0 +1,74 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.Date;
+
+@Entity
+@Table(name = "Recipes")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long recipeId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false, length = 255)
+    private String title;
+    
+    @Column(nullable = false, length = 255)
+    private String cookingMethod;
+    
+    @Column(nullable = false, length = 255)
+    private String category;
+    
+    @Column(nullable = false, length = 255)
+    private String weight;
+    
+    @Column(nullable = false, length = 255)
+    private int energy;
+    
+    @Column(nullable = false, length = 255)
+    private float carbohydrate;
+    
+    @Column(nullable = false, length = 255)
+    private float protein;
+    
+    @Column(nullable = false, length = 255)
+    private float fat;
+    
+    @Column(nullable = false, length = 255)
+    private float sodium;
+    
+    @Column(nullable = false, length = 255)
+    private String hashTag;
+    
+    @Column(nullable = false, length = 255)
+    private String imageSmall;
+    
+    @Column(nullable = false, length = 255)
+    private String imageLarge;
+    
+    @Column(nullable = false, length = 255)
+    private String ingredients;
+    
+    @Column(columnDefinition = "TEXT")
+    private String tip;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt = new Date();
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedAt = new Date();
+    
+    @Column(nullable = false)
+    private boolean deletedYn = false;
+}
